3c081e344263bb5f68aa1c42a4a90cc468399fa2
Ron Melendres <Ron.melendres25@gmail.com>
2019-10-31 17:36:42 -0700

    added pages folder in src

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,36 +1,32 @@
 import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
+import { BrowserRouter as Router } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
 import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import Form from "./components/Form";
-import Wrapper from "./components/Wrapper";
+// import Wrapper from "./components/Wrapper";
 import Cards from "./components/Cards";
 
 function App() {
     return (
-        <Wrapper>
-
-            <NavBar />
-            <Jumbotron />
-            <Form />
-
-            <Cards>
-
-            </Cards>
-        </Wrapper>
-
+        <Router>
+            <div>
+                <NavBar />
+                <Jumbotron />
+                <Form />
+                <Cards>
+                    <Switch>
+                        <Route exact path="/" component={Books} />
+                        <Route exact path="/books" component={Books} />
+                        <Route exact path="/books/:id" component={Detail} />
+                        <Route component={NoMatch} />
+                    </Switch>
+                </Cards>
+            </div>
+        </Router>
     );
 }
 export default App;
 
-// <Router></Router
-// Route, Switch (in browser router)
-/* <Switch>
-    <Route exact path="/" component={Books} />
-    <Route exact path="/books" component={Books} />
-    <Route exact path="/books/:id" component={Detail} />
-    <Route component={NoMatch} />
-</Switch> */
--- create-react-express/client/src/components/Wrapper/index.js
+++ create-react-express/client/src/components/Wrapper/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+// import React from "react";
 // import "./WrapperStyle.css";
 
-function Wrapper(props) {
-    return <div className="wrapper">{props.children}</div>
-}
+// function Wrapper(props) {
+//     return <div className="wrapper">{props.children}</div>
+// }
 
-export default Wrapper; 
+// export default Wrapper; 
